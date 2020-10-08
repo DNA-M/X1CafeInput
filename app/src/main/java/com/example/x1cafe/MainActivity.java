d84f714afbd6ea81e7207ea9888b4852b76380fe
devk1903 <devikarlina1903@gmail.com>
2020-10-08 11:24:49 +0700

    komit lgiiiiii

--- app/src/main/java/com/example/x1cafe/MainActivity.java
+++ app/src/main/java/com/example/x1cafe/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+//komen lgi deh
 
 public class MainActivity extends AppCompatActivity {
 
