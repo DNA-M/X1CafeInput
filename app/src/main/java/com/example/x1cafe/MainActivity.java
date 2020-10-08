9c21e6c596c649c2519df91c91b249bee2ce4f4b
devk1903 <devikarlina1903@gmail.com>
2020-10-08 11:23:08 +0700

    komit

--- app/src/main/java/com/example/x1cafe/MainActivity.java
+++ app/src/main/java/com/example/x1cafe/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-//komentttt
 
 public class MainActivity extends AppCompatActivity {
 
