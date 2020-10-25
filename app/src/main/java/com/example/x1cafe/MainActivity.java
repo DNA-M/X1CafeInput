13e1f4655116db97024417831f99daf4ca8bc931
devk1903 <devikarlina1903@gmail.com>
2020-10-25 13:51:23 +0700

    tgl 25

--- app/src/main/java/com/example/x1cafe/MainActivity.java
+++ app/src/main/java/com/example/x1cafe/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 //komen lgi deh
+// komit tgl 25 oktober 2020
 
 public class MainActivity extends AppCompatActivity {
 
